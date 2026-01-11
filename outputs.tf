03ddc8d057a06e4ec724b3da26c9aaa075e44fdd
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:22:57 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "web_pubsub_custom_certificates" {
+  description = "All web_pubsub_custom_certificate resources"
+  value       = azurerm_web_pubsub_custom_certificate.web_pubsub_custom_certificates
+}
+
