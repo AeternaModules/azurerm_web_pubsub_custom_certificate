2494d37768a4d64e93faf78d8387bfbe8e229a6e
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:16:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "web_pubsub_custom_certificates" {
   description = "All web_pubsub_custom_certificate resources"
   value       = azurerm_web_pubsub_custom_certificate.web_pubsub_custom_certificates
 }
+output "web_pubsub_custom_certificates_certificate_version" {
+  description = "List of certificate_version values across all web_pubsub_custom_certificates"
+  value       = [for k, v in azurerm_web_pubsub_custom_certificate.web_pubsub_custom_certificates : v.certificate_version]
+}
+output "web_pubsub_custom_certificates_custom_certificate_id" {
+  description = "List of custom_certificate_id values across all web_pubsub_custom_certificates"
+  value       = [for k, v in azurerm_web_pubsub_custom_certificate.web_pubsub_custom_certificates : v.custom_certificate_id]
+}
+output "web_pubsub_custom_certificates_name" {
+  description = "List of name values across all web_pubsub_custom_certificates"
+  value       = [for k, v in azurerm_web_pubsub_custom_certificate.web_pubsub_custom_certificates : v.name]
+}
+output "web_pubsub_custom_certificates_web_pubsub_id" {
+  description = "List of web_pubsub_id values across all web_pubsub_custom_certificates"
+  value       = [for k, v in azurerm_web_pubsub_custom_certificate.web_pubsub_custom_certificates : v.web_pubsub_id]
+}
 
